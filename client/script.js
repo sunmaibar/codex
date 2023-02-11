1f87629d5065224ab7d96d6fe3a9baa2136c2479
Jimmy <jajalaba@yahoo.com.tw>
2023-02-11 13:03:26 +0800

    3qr

--- client/script.js
+++ client/script.js
@@ -64,7 +64,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   // fetch data
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-8oeq.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
